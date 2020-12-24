0d00f1c3a6921a9500d0cd139bedc4eccec20cee
danjarosz <jarosz.daniel@protonmail.com>
2020-12-24 13:01:58 +0100

    feat(Square): convert to function component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,15 +1,9 @@
 import React from "react";
 
-class Square extends React.Component {
-  render() {
-    const { value, onClick } = this.props;
-
-    return (
-      <button className="square" onClick={onClick}>
-        {value}
-      </button>
-    );
-  }
-}
+const Square = ({ value, onClick }) => (
+  <button className="square" onClick={onClick}>
+    {value}
+  </button>
+);
 
 export default Square;
